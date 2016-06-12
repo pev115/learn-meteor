3861cefd29e670b92ab81be6a06f924b9530cc0a
pev <pe.val@icloud.com>
2016-06-12 16:46:42 +0000

    5.3

--- microscope/lib/router.js
+++ microscope/lib/router.js
@@ -1,4 +1,4 @@
-/*global Router*/
+/*global Router,Posts*/
 
 Router.configure({
 
@@ -9,3 +9,7 @@ waitOn: function() {return Meteor.subscribe('posts');}
 });
 
 Router.route('/', {name: 'postsList'});
+Router.route('/posts/:_id',{
+    name: 'postPage',
+    data: function() {return Posts.findOne(this.params._id);}
+});
