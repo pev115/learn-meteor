d5294879dc87c4314f6ee47d2e370906fab750e8
pev <pe.val@icloud.com>
2016-06-13 16:09:53 +0000

    7.1

--- microscope/lib/router.js
+++ microscope/lib/router.js
@@ -1,19 +1,19 @@
 /*global Router,Posts*/
 
 Router.configure({
-
-layoutTemplate: 'layout',
-loadindTemplate : 'loading',
-notFoundTemplate : 'notFound',
-waitOn: function() {return Meteor.subscribe('posts');}
-
-
+    layoutTemplate: 'layout',
+    loadindTemplate : 'loading',
+    notFoundTemplate : 'notFound',
+    waitOn: function() {return Meteor.subscribe('posts');}
 });
 
 Router.route('/', {name: 'postsList'});
+
 Router.route('/posts/:_id',{
     name: 'postPage',
     data: function() {return Posts.findOne(this.params._id);}
 });
+
+Router.route('/submit',{name:'postSubmit'});
 
 Router.onBeforeAction('dataNotFound', {only: 'postPage'});
