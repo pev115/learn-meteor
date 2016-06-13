08ce4340f917e4bfa91f2db9dec8995543cfe7a3
pev <pe.val@icloud.com>
2016-06-13 18:37:27 +0000

    7.3

--- microscope/lib/collections/posts.js
+++ microscope/lib/collections/posts.js
@@ -1,2 +1,9 @@
 /*global Posts*/
 Posts = new Mongo.Collection('posts');
+
+Posts.allow({
+    insert: function(userId, doc){
+        //only allow posting if you are logged in.
+        return !! userId;
+    }
+});
