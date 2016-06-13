25d7fb92c70320120c3800a81b711409b1f2d188
pev <pe.val@icloud.com>
2016-06-13 19:30:14 +0000

    7.6

--- microscope/client/templates/posts/post_submit.js
+++ microscope/client/templates/posts/post_submit.js
@@ -6,8 +6,13 @@ Template.postSubmit.events({
             url: $(e.target).find('[name=url]').val(),
             title: $(e.target).find('[name=title]').val()
         };
+ 
+    Meteor.call('postInsert', post, function(error, result){
+        //display the error to the user and abort
+        if(error)
+            return alert(error.reason);
         
-        post._id = Posts.insert(post);
-        Router.go('postPage', post);
+        Router.go('postPage',{_id:result._id});
+    }) ;
     }
 });
